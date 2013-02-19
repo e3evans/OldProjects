8c459a4f3064f90acb792db7ada3d8dfad42aa9e
Billy Haga <billy.haga@aurora.org>
2013-02-19 11:33:24 -0600

    bh - project files, updated build, fixed logging and standards

--- CaregiverLogin/src/com/aurora/controllers/LoginEditController.java
+++ CaregiverLogin/src/com/aurora/controllers/LoginEditController.java
@@ -11,6 +11,7 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.ValidatorException;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,44 +20,46 @@ import org.springframework.web.portlet.bind.annotation.ActionMapping;
 
 import com.aurora.org.caregiverlogin.forms.LoginEditForm;
 
-
 @Controller
 @RequestMapping("edit_defaults")
 public class LoginEditController {
-	
+
+	protected static final Logger log = Logger
+			.getLogger(LoginEditController.class.getSimpleName());
+
+	@SuppressWarnings("rawtypes")
 	@RequestMapping
-	public ModelAndView defaultEditView(RenderRequest request, RenderResponse response, @SuppressWarnings("rawtypes") Map Model,@ModelAttribute("editForm")LoginEditForm editForm){
-		
-		if (editForm==null)editForm=new LoginEditForm();
+	public ModelAndView defaultEditView(RenderRequest request,
+			RenderResponse response, Map Model,
+			@ModelAttribute("editForm") LoginEditForm editForm) {
+		if (editForm == null)
+			editForm = new LoginEditForm();
 		PortletPreferences prefs = request.getPreferences();
-		editForm.setWcm_menuComponent(prefs.getValue(LoginViewController.PREF_WCM_COMPONENT, ""));
-		editForm.setWcm_path(prefs.getValue(LoginViewController.PREF_WCM_PATH, ""));
-		editForm.setWcm_library(prefs.getValue(LoginViewController.PRED_WCM_LIB, ""));
-	
-		return new ModelAndView("loginEdit","editForm",editForm);
+		editForm.setWcm_menuComponent(prefs.getValue(
+				LoginViewController.PREF_WCM_COMPONENT, ""));
+		editForm.setWcm_path(prefs.getValue(LoginViewController.PREF_WCM_PATH,
+				""));
+		editForm.setWcm_library(prefs.getValue(
+				LoginViewController.PRED_WCM_LIB, ""));
+		return new ModelAndView("loginEdit", "editForm", editForm);
 	}
-	
+
 	@ActionMapping("doSavePrefs")
-	public void savePrefs(ActionRequest request, ActionResponse response,@ModelAttribute("editForm")LoginEditForm editForm){
+	public void savePrefs(ActionRequest request, ActionResponse response,
+			@ModelAttribute("editForm") LoginEditForm editForm) {
 		PortletPreferences prefs = request.getPreferences();
 		Map<String, String> formPrefs = editForm.getPreferences();
 		try {
-			for (Map.Entry<String, String>entry:formPrefs.entrySet()){
+			for (Map.Entry<String, String> entry : formPrefs.entrySet()) {
 				prefs.setValue(entry.getKey(), entry.getValue());
 			}
 			prefs.store();
 		} catch (ReadOnlyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("ReadOnlyException in savePrefs", e);
 		} catch (ValidatorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("ValidatorException in savePrefs", e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("IOException in savePrefs", e);
 		}
-		
-		
 	}
-
 }
--- CaregiverLogin/src/com/aurora/controllers/LoginViewController.java
+++ CaregiverLogin/src/com/aurora/controllers/LoginViewController.java
@@ -14,6 +14,7 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,69 +27,78 @@ import com.ibm.portal.portlet.service.PortletServiceUnavailableException;
 import com.ibm.portal.portlet.service.login.LoginHome;
 import com.ibm.portal.portlet.service.login.LoginService;
 
-
-
-
 @Controller
 @RequestMapping("VIEW")
 public class LoginViewController {
-	
+
+	protected static final Logger log = Logger
+			.getLogger(LoginViewController.class.getSimpleName());
+
 	public static String ENV_USERPASSWORD = "com.aurora.seedlist.userpassword";
 	public static String PREF_WCM_PATH = "wcm.path";
-	public static String PREF_WCM_COMPONENT ="wcm.menuComponent";
+	public static String PREF_WCM_COMPONENT = "wcm.menuComponent";
 	public static String PRED_WCM_LIB = "wcm.library";
 	public static String PARAM_BAD_SESSION = "SESSIONTIMEOUT";
 	public boolean BAD_LOGIN = false;
 	public boolean BAD_SESSION = false;
-	
+
 	LoginHome loginHome;
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		try {
 			InitialContext ctx = new InitialContext();
-			PortletServiceHome psh = (PortletServiceHome)ctx.lookup(LoginHome.JNDI_NAME);
-			loginHome = (LoginHome)psh.getPortletService(LoginHome.class);
+			PortletServiceHome psh = (PortletServiceHome) ctx
+					.lookup(LoginHome.JNDI_NAME);
+			loginHome = (LoginHome) psh.getPortletService(LoginHome.class);
 		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("NamingException in init", e);
 		} catch (PortletServiceUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("PortletServiceUnavailableException in init", e);
 		}
 	}
-	
 
+	@SuppressWarnings("rawtypes")
 	@RequestMapping
-	public ModelAndView defaultView (RenderRequest request, RenderResponse responses, @SuppressWarnings("rawtypes") Map model,@ModelAttribute("loginForm")LoginForm form) throws UnsupportedEncodingException{
-		HttpServletRequest hsreq= com.ibm.ws.portletcontainer.portlet.PortletUtils.getHttpServletRequest(request);
-		if (null!=hsreq.getParameter(PARAM_BAD_SESSION) && !BAD_LOGIN){
-			BAD_SESSION=true;
-		}else{
-			BAD_SESSION=false;
+	public ModelAndView defaultView(RenderRequest request,
+			RenderResponse responses, Map model,
+			@ModelAttribute("loginForm") LoginForm form)
+			throws UnsupportedEncodingException {
+		HttpServletRequest hsreq = com.ibm.ws.portletcontainer.portlet.PortletUtils
+				.getHttpServletRequest(request);
+		if (null != hsreq.getParameter(PARAM_BAD_SESSION) && !BAD_LOGIN) {
+			BAD_SESSION = true;
+		} else {
+			BAD_SESSION = false;
 		}
-		if (form==null)form = new LoginForm();
+		if (form == null)
+			form = new LoginForm();
 		form.setBadLogin(BAD_LOGIN);
 		form.setBadSession(BAD_SESSION);
-		return new ModelAndView("loginView","loginForm",form);
+		return new ModelAndView("loginView", "loginForm", form);
 	}
-	
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@ActionMapping("doLogin")
-	public void doLogin(ActionRequest request, ActionResponse response,@ModelAttribute("loginForm")LoginForm loginForm) throws IOException{
-		LoginService loginService = loginHome.getLoginService(request, response);
+	public void doLogin(ActionRequest request, ActionResponse response,
+			@ModelAttribute("loginForm") LoginForm loginForm)
+			throws IOException {
+		LoginService loginService = loginHome
+				.getLoginService(request, response);
 		Map contextMap = new HashMap();
-        contextMap.put(LoginService.DO_RESUME_SESSION_KEY, new Boolean(false));
+		contextMap.put(LoginService.DO_RESUME_SESSION_KEY, new Boolean(false));
 
-		BAD_LOGIN=false;
+		BAD_LOGIN = false;
 		try {
-			loginService.login(loginForm.getUserName(), loginForm.getPassword().toCharArray(), contextMap, null);
-		} catch (Exception e){
-			BAD_LOGIN=true;
-		}finally{
-			if (!BAD_LOGIN)response.sendRedirect("/cgc/myportal/connect/home");
+			loginService.login(loginForm.getUserName(), loginForm.getPassword()
+					.toCharArray(), contextMap, null);
+			log.info(loginForm.getUserName());
+		} catch (Exception e) {
+			BAD_LOGIN = true;
+			log.error("Exception in doLogin", e);
+		} finally {
+			if (!BAD_LOGIN)
+				response.sendRedirect("/cgc/myportal/connect/home");
 		}
-		
 	}
-
 }
