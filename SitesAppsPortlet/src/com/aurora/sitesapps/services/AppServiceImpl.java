eeefd1cfb875ded992ec1970bf6ba9b209745912
Billy Haga <billy.haga@aurora.org>
2013-02-27 09:05:28 -0600

    Merging Tir's and my branches resulted in a few lost changes I had made for using the system prop to determine base domain url vs hard coding in app and having to build wars for each env. This was fixed and I confirmed that the auto filter searching is now working for sitesapps.

--- SitesAppsPortlet/src/com/aurora/sitesapps/services/AppServiceImpl.java
+++ SitesAppsPortlet/src/com/aurora/sitesapps/services/AppServiceImpl.java
@@ -2,94 +2,98 @@ package com.aurora.sitesapps.services;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.aurora.sitesapps.beans.AppCategory;
 import com.aurora.sitesapps.beans.Application;
 import com.aurora.sitesapps.delegates.ServiceInterfaceDelegate;
 import com.aurora.sitesapps.exception.AppException;
-import com.aurora.sitesapps.util.ResourceUtil;
 
 @Service(value = "appService")
 public class AppServiceImpl implements AppService {
+
 	private Logger logger = Logger.getLogger(AppServiceImpl.class);
+
 	@Autowired
 	private ServiceInterfaceDelegate serviceInterfaceDelegate;
-	
-	
-	
+
+	protected String SERVICE_URL = "/QuickLinksServiceApp/rest/qlservice/";
+	protected String CATEGORY_LIST = SERVICE_URL + "appcategorylist";
+	protected String APP_LIST_CATEGORY_KEY = SERVICE_URL + "applistbycategory/";
+	protected String POPULAR_CATEGORY_LIST = SERVICE_URL
+			+ "popularlistbycategory/";
+	protected String AVAILABLE_APP_LIST = SERVICE_URL + "results";
+
 	/**
 	 * 
-	 * This method Get the all available Application by Category Id.
+	 * This method gets all available Application by Category Id.
 	 * 
 	 */
-
-	public List<Application> listApplication(String categoryId) throws AppException{
+	public List<Application> listApplication(String categoryId)
+			throws AppException {
 		List<Application> appList = null;
 		Application app = null;
 		try {
 			appList = new ArrayList<Application>();
-			
-			String requestPath =  ResourceUtil.getApplistbycategory()+categoryId;
-			
-		    logger.error("webservice call starts with request path "+requestPath);
+			String requestPath = APP_LIST_CATEGORY_KEY + categoryId;
+			logger.info("webservice call starts with request path "
+					+ requestPath);
 			JSONObject allAvailAppJson = new JSONObject(
 					serviceInterfaceDelegate.processGetRestRequest(requestPath));
-
-			logger.error("webservice call ends starts with request path "+requestPath);
-			JSONArray jsonArray = allAvailAppJson.getJSONArray("applicationList");
+			logger.info("webservice call ends starts with request path "
+					+ requestPath);
+			JSONArray jsonArray = allAvailAppJson
+					.getJSONArray("applicationList");
 			for (int i = 0; i < jsonArray.length(); i++) {
 				app = new Application();
 				JSONObject jsonobj = jsonArray.getJSONObject(i);
-				
-				String description =jsonobj.get("appDesc").toString();
-				if(description.length()>50){
-					description=description.substring(0, 50)+"...";
-					
+
+				String description = jsonobj.get("appDesc").toString();
+				if (description.length() > 50) {
+					description = description.substring(0, 50) + "...";
 				}
-				
-				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo").toString());
-				app.setAppId(jsonobj.getJSONObject("appKey").get("appId").toString());
+
+				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo")
+						.toString());
+				app.setAppId(jsonobj.getJSONObject("appKey").get("appId")
+						.toString());
 				app.setAppDesc(description);
 				app.setAppName(jsonobj.get("appName").toString().trim());
 				app.setAppURL((jsonobj.get("appURL").toString()).trim());
-				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString()).trim());
+				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString())
+						.trim());
 				appList.add(app);
 			}
 
-		}
-		catch (JSONException je) {
+		} catch (JSONException je) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation !!!!"+je);
-			logger.error("Exception in listUserAppByUserId !!!  "+je);
+			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation - "
+					+ je);
+			logger.error("Exception in listUserAppByUserId", je);
 			throw ae;
-			
-		}
-		catch (AppException ae) {
-			throw ae;
-			
-		}catch (Exception e) {
-			AppException ae = new AppException();
-			ae.setExceptionType("Service End Point");
-			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in Service Implmentation !!!!"+e);
-			logger.error("Exception in listApplication !!!  "+e);
-			throw ae;
-			
-		}
 
+		} catch (AppException ae) {
+			throw ae;
+
+		} catch (Exception e) {
+			AppException ae = new AppException();
+			ae.setExceptionType("Service End Point");
+			ae.setExceptionCode("QLEXception 001");
+			ae.setExceptionMessage("Excetion in Service Implmentation - " + e);
+			logger.error("Exception in listApplication", e);
+			throw ae;
+
+		}
 		return appList;
-
 	}
-
-
-
 
 	@Override
 	public List<AppCategory> listAppCategory() throws AppException {
@@ -97,56 +101,47 @@ public class AppServiceImpl implements AppService {
 		AppCategory appCategory = null;
 		try {
 			categoryList = new ArrayList<AppCategory>();
-			
-			String requestPath =  ResourceUtil.getCategorylisturl();
-			
-			logger.error("webservice call starts with request path "+requestPath);
+			String requestPath = CATEGORY_LIST;
+			logger.info("webservice call starts with request path "
+					+ requestPath);
 			JSONObject allAvailAppJson = new JSONObject(
 					serviceInterfaceDelegate.processGetRestRequest(requestPath));
 
-			logger.error("webservice call ends starts with request path "+requestPath);
-			JSONArray jsonArray = allAvailAppJson.getJSONArray("appCategoryList");
+			logger.info("webservice call ends starts with request path "
+					+ requestPath);
+			JSONArray jsonArray = allAvailAppJson
+					.getJSONArray("appCategoryList");
 			for (int i = 0; i < jsonArray.length(); i++) {
 				appCategory = new AppCategory();
 				JSONObject jsonobj = jsonArray.getJSONObject(i);
-				
-				
-				
-				appCategory.setCategoryId((jsonobj.get("appCategoryId").toString()));
-				appCategory.setCategoryName(((jsonobj.get("appCategoryName").toString())));
+				appCategory.setCategoryId((jsonobj.get("appCategoryId")
+						.toString()));
+				appCategory.setCategoryName(((jsonobj.get("appCategoryName")
+						.toString())));
 				categoryList.add(appCategory);
 			}
-
-		}
-		catch (JSONException je) {
+		} catch (JSONException je) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation !!!!"+je);
-			logger.error("Exception in listUserAppByUserId !!!  "+je);
+			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation - "
+					+ je);
+			logger.error("Exception in listUserAppByUserId", je);
 			throw ae;
-			
-		}
-		catch (AppException ae) {
+		} catch (AppException ae) {
 			throw ae;
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in Service Implmentation !!!!"+e);
-			logger.error("Exception in listApplication !!!  "+e);
+			ae.setExceptionMessage("Excetion in Service Implmentation - " + e);
+			logger.error("Exception in listApplication", e);
 			throw ae;
-			
-		}
 
+		}
 		return categoryList;
-
-	
-
 	}
-
-
 
 	/**
 	 * 
@@ -154,130 +149,111 @@ public class AppServiceImpl implements AppService {
 	 * 
 	 */
 
-	public List<Application> listPopularApplication(String categoryId) throws AppException{
+	public List<Application> listPopularApplication(String categoryId)
+			throws AppException {
 		List<Application> appList = null;
 		Application app = null;
 		try {
 			appList = new ArrayList<Application>();
-			
-			String requestPath =  ResourceUtil.getPopularapplistbycategory()+categoryId;
-			
-			logger.error("webservice call starts with request path "+requestPath);
+			String requestPath = POPULAR_CATEGORY_LIST + categoryId;
+			logger.info("webservice call starts with request path "
+					+ requestPath);
 			JSONObject allAvailAppJson = new JSONObject(
 					serviceInterfaceDelegate.processGetRestRequest(requestPath));
-
-			logger.error("webservice call ends starts with request path "+requestPath);
-			JSONArray jsonArray = allAvailAppJson.getJSONArray("applicationList");
+			logger.info("webservice call ends starts with request path "
+					+ requestPath);
+			JSONArray jsonArray = allAvailAppJson
+					.getJSONArray("applicationList");
 			for (int i = 0; i < jsonArray.length(); i++) {
 				app = new Application();
 				JSONObject jsonobj = jsonArray.getJSONObject(i);
-				
-				String description =jsonobj.get("appDesc").toString();
-				if(description.length()>50){
-					description=description.substring(0, 50)+"...";
-					
+				String description = jsonobj.get("appDesc").toString();
+				if (description.length() > 50) {
+					description = description.substring(0, 50) + "...";
 				}
-				
-				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo").toString());
-				app.setAppId(jsonobj.getJSONObject("appKey").get("appId").toString());
+				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo")
+						.toString());
+				app.setAppId(jsonobj.getJSONObject("appKey").get("appId")
+						.toString());
 				app.setAppDesc(description);
 				app.setAppName(jsonobj.get("appName").toString().trim());
 				app.setAppURL((jsonobj.get("appURL").toString()).trim());
-				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString()).trim());
+				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString())
+						.trim());
 				appList.add(app);
 			}
 
-		}
-		catch (JSONException je) {
+		} catch (JSONException je) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation !!!!"+je);
-			logger.error("Exception in listUserAppByUserId !!!  "+je);
+			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation - "
+					+ je);
+			logger.error("Exception in listUserAppByUserId", je);
 			throw ae;
-			
-		}
-		catch (AppException ae) {
+		} catch (AppException ae) {
 			throw ae;
-			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in Service Implmentation !!!!"+e);
-			logger.error("Exception in listApplication !!!  "+e);
+			ae.setExceptionMessage("Excetion in Service Implmentation - " + e);
+			logger.error("Exception in listApplication", e);
 			throw ae;
-			
 		}
-
 		return appList;
-
 	}
 
-	
-	
-	public List<Application> listApplication() throws AppException{
+	public List<Application> listApplication() throws AppException {
 		List<Application> appList = null;
 		Application app = null;
 		try {
 			appList = new ArrayList<Application>();
-			
-			String requestPath =  ResourceUtil.getAvailableapplisturl();
-			
-			logger.error("webservice call starts with request path "+requestPath);
+			String requestPath = AVAILABLE_APP_LIST;
+			logger.info("webservice call starts with request path "
+					+ requestPath);
 			JSONObject allAvailAppJson = new JSONObject(
 					serviceInterfaceDelegate.processGetRestRequest(requestPath));
-
-			logger.error("webservice call ends starts with request path "+requestPath);
-			JSONArray jsonArray = allAvailAppJson.getJSONArray("applicationList");
+			logger.info("webservice call ends starts with request path "
+					+ requestPath);
+			JSONArray jsonArray = allAvailAppJson
+					.getJSONArray("applicationList");
 			for (int i = 0; i < jsonArray.length(); i++) {
 				app = new Application();
 				JSONObject jsonobj = jsonArray.getJSONObject(i);
-				
-				String description =jsonobj.get("appDesc").toString();
-				if(description.length()>50){
-					description=description.substring(0, 50)+"...";
-					
+				String description = jsonobj.get("appDesc").toString();
+				if (description.length() > 50) {
+					description = description.substring(0, 50) + "...";
 				}
-				
-				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo").toString());
-				app.setAppId(jsonobj.getJSONObject("appKey").get("appId").toString());
+				app.setSeqNo(jsonobj.getJSONObject("appKey").get("seqNo")
+						.toString());
+				app.setAppId(jsonobj.getJSONObject("appKey").get("appId")
+						.toString());
 				app.setAppDesc(description);
 				app.setAppName(jsonobj.get("appName").toString().trim());
 				app.setAppURL((jsonobj.get("appURL").toString()).trim());
-				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString()).trim());
+				app.setLoggedInAccess((jsonobj.get("loggedInAccess").toString())
+						.trim());
 				appList.add(app);
 			}
-
-		}
-		catch (JSONException je) {
+		} catch (JSONException je) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation !!!!"+je);
-			logger.error("Exception in listUserAppByUserId !!!  "+je);
+			ae.setExceptionMessage("Excetion in JSON PArsing Service Implmentation - "
+					+ je);
+			logger.error("Exception in listUserAppByUserId", je);
 			throw ae;
-			
-		}
-		catch (AppException ae) {
+		} catch (AppException ae) {
 			throw ae;
-			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			AppException ae = new AppException();
 			ae.setExceptionType("Service End Point");
 			ae.setExceptionCode("QLEXception 001");
-			ae.setExceptionMessage("Excetion in Service Implmentation !!!!"+e);
-			logger.error("Exception in listApplication !!!  "+e);
+			ae.setExceptionMessage("Excetion in Service Implmentation - " + e);
+			logger.error("Exception in listApplication", e);
 			throw ae;
-			
 		}
-		
-
-
 		return appList;
-
 	}
-
-
 }
-
